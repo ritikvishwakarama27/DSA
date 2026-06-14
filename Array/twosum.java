7c5a100df9d99abfc5a5bf00b98827e290288ac8
RITIK VISHWAKARMA <ritikvishwakarma306@gmail.com>
2026-06-14 19:52:41 +0530

    Add implementation for two-sum problem

---
+++ Array/twosum.java
@@ -0,0 +1,25 @@
+public class twosum {
+    public static int[] twoSum(int[] nums, int target) {
+        java.util.Map<Integer, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("[" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution");
+        }
+    }
+}
